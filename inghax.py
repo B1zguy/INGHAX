b0b2377aeda0abd2f5372d4803757834226db2d1
Nathan <nathan@jesui.net>
2021-02-15 18:15:43 +1100

    Added comments for clarity. Fixed issue with headless parameter. Updated gitignore.

--- inghax.py
+++ inghax.py
@@ -33,14 +33,6 @@ def browserCheck(x):
         stringOnly(x)
         return x
 
-'''def boolCheck(x):
-    print(x)
-    try:
-        bool(x)
-    except ValueError:
-        return x
-    raise argparse.ArgumentTypeError('Keep it True or False!')'''
-
 def boolCheck(x):
     try:
         return ast.literal_eval(x)
@@ -82,17 +74,20 @@ if inputs.browser == 'Firefox':   # M A S T E R R A C E
     from selenium.webdriver import Firefox
     from selenium.webdriver.firefox.options import Options
     opts = Options()
+    if headless == True:
+        opts.add_argument("--headless")
     #opts.add_argument("--headless")
     browser = Firefox(options=opts)
-else:  # Yeah I can probs fix how I'm asking for input since one name is reduntant. Already checking input earlier up
+else:  # Yeah I can probs fix how I'm asking for input since one name is redundant. Already checking input earlier up
     from selenium.webdriver import Chrome
     from selenium.webdriver.chrome.options import Options
     opts = Options()
+    if headless == True:
+        opts.add_argument("--headless")
     #opts.add_argument("--headless")
     browser = Chrome(options=opts)
 
-if headless == True:
-    opts.add_argument("--headless")
+
 browser.get("https://ing.com.au/securebanking/")
 
 def banner():
@@ -115,19 +110,21 @@ I::::::::IN::::::N        N::::::N     GGG::::::GGG:::GH:::::::H     H:::::::H A
 IIIIIIIIIINNNNNNNN         NNNNNNN        GGGGGG   GGGGHHHHHHHHH     HHHHHHHHHAAAAAAA                   AAAAAAAXXXXXXX       XXXXXXX
 """)
 
-# Nathan is a cuck.
-
+# Compares 0 on keypad to template.png
+# EasyOCR cannot automatically detect a 0
+# Similar to save function
 def template_match(data):
-    #print(data)
     clean_base64 = base64.b64decode((str(data)))
     narray = np.frombuffer(clean_base64, np.uint8)
     img = cv2.imdecode(narray, cv2.IMREAD_ANYCOLOR)
+    # Zero_image is what was found on the ING keypad
+    # Generated at every runtime
     cv2.imwrite("Zero_image.png", img)
     zero = cv2.imread("Zero_image.png")
     template = cv2.imread("template.png")
+    # OCR comparison done here
+    # Res returns with a probability in a np array
     res = cv2.matchTemplate(zero, template, cv2.TM_CCOEFF_NORMED)
-    print(res)
-    print(type(res))
     if res >= 0.60:
         print("Probs 0")
         return "0"
@@ -135,40 +132,50 @@ def template_match(data):
         print("WTF")
         return 1
 
-# Nathan is a cuck.
+# Bulk of determining what keys are what happens here
 def save(encoded_data, file):
     clean_base64 = base64.b64decode((str(encoded_data)))
     narray = np.frombuffer(clean_base64, np.uint8)
     img = cv2.imdecode(narray, cv2.IMREAD_ANYCOLOR)
+    # Writes img file from base64 encode
     cv2.imwrite(file, img)
+    # Reader (OCR) analyses the img file just created
     result = reader.readtext(file)
     try:
         value = result[-0][1]
         # print("Digit: {0}".format(value))
         # print("Base64: {0}".format(encoded_data))
         lukeisafucboi[value] = encoded_data
+    # Catching invalid slices because result was empty (ie. OCR couldn't determine keypad number)
     except IndexError:
-        #print("Testing")
+        # The base64 encodes that couldn't be solved get sent for specialist template matching
         value2 = template_match(encoded_data)
         if value2 == "0":
+            # Input the determined 0 keypad into existing dictionary
             lukeisafucboi[value2] = encoded_data
 
-
+# Ensures page has loaded
 time.sleep(3)
+# Finds and inputs Client Number into field (just plain text input here)
 table = browser.find_element_by_xpath("""//*[@id="cifField"]""")
 browser.find_element_by_id("cifField").clear()
 browser.find_element_by_id("cifField").send_keys(str(clientNumber))
 
+# Grabs all base64 imgs that makeup the ING keypad
 digitpanel = browser.find_element_by_xpath("""//*[@id="keypad"]""")
 keypad = digitpanel.find_element_by_class_name("module-keypad")
+# Find and store location of login btn for later
 login_button = browser.find_element_by_xpath("""//*[@id="login-btn"]""")
 ing_values = {}
 keypad_digits = {}
 
+# Dictionary pairing OCR'd digits and its corresponding base64 from the webpage
 lukeisafucboi = {}
+# Dictionary pairing base64 with its div location that it was found in
+## Div location is just the index representing its nesting, within Selenium (not webpage)
 lukehasmallpp = {}
 
-
+# The OCR system that does the heavy lifting
 reader = easyocr.Reader(['en'])
 
 
@@ -181,11 +188,14 @@ banner()
 # 1, 4  div elements = 0 isnt in the elements. - I am god.
 for i in range(0, 5):
     for j in range(0, 5):
+        # Iterate through keypad digits
         for key_num in keypad.find_elements_by_xpath("""//*[@id="keypad"]/div/div[{}]/div[{}]/div/img""".format(j, i)):
+            # Src is where the base64 is stored on the page
             digits = key_num.get_attribute("src")
             ing_values = digits
             #print(ing_values[22:])
             test_vales = ing_values[22:]
+            # Store each key's base64 and corresponding div location for later
             lukehasmallpp[test_vales] = (j, i)
             save(test_vales, "testfile{0}.png".format(i))
 
@@ -194,17 +204,21 @@ for i in range(0, 5):
 #print(lukehasmallpp)
 #print('\n \n')
 
+# Dictionary pairing base64 w/ its location
+# Made by comparing lukeisafucboi & lukehasmallpp to determined keypaid location for desired PIN
 listy = []
 '''for senpaiLukas in PIN:
     rr = lukeisafucboi[senpaiLukas]
     listy[rr] = lukehasmallpp[rr]
 pprint(listy)'''
 
+# Iterate through dictionary of determined digits and dictionary of known location
+# to find locations of PIN's numbers
 for senpaiLukas in list(PIN):
     imgString = lukeisafucboi[senpaiLukas]
     listy.append([imgString, lukehasmallpp[imgString]])
-#pprint(listy)
 
+# Click each keypad button, drawing from determined list
 for i in listy:
     nums = i[1]
     position = keypad.find_elements_by_xpath("""//*[@id="keypad"]/div/div[{}]/div[{}]/div/img""".format(nums[0], nums[1]))[0]
@@ -214,8 +228,12 @@ for i in listy:
     print(i)
     print(i.get_attribute("value"))'''
 
+print('listy')
 pprint(listy)
+print('fucboi')
 pprint(lukeisafucboi)
+print('pp')
+pprint(lukehasmallpp)
 
 
 time.sleep(1)
